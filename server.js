8d649d76a31db081dcfbb994ceca102b6a0fd63b
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2021-01-12 22:12:41 +0000

    addind port as env

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.post('/image', (req, res) => {
     handleClarifaiApiCall(req, res)
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
